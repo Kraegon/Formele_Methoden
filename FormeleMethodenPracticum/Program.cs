b2409cf6359f59659beb4cdd4d7d033f0b6b817b
Thom Trignol <tgtrignol@gmail.com>
2015-05-13 12:38:02 +0200

    Created UI.

--- FormeleMethodenPracticum/Program.cs
+++ FormeleMethodenPracticum/Program.cs
@@ -16,7 +16,7 @@ namespace FormeleMethodenPracticum
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
