83a2b02711d5646fa0252d8abe171449a120c68e
Kraegon <Jgwest@zaku2.com>
2015-05-19 20:40:17 +0200

    Added commentary to regex stub

---
+++ FormeleMethodenPracticum/model/regex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace FormeleMethodenPracticum.model
+{
+    /**
+     * Let's do regex!
+     * The following characters make up our language
+     *  '$'         := The empty string character
+     *  '|'         := The OR operator
+     *  '*'         := The zero-or-more operator
+     *  '+'         := The one-or-more operator
+     *  '{' & '}'   := The collections brackets
+     *  '(' & ')'   := Your run-off-the-mill bracketty brackets.
+     */
+    static class Regex
+    {
+        private static const char[] validCharacters = {'$', '|','*','+','{','}','(',')'}; //And normal letters/numerals for language
+
+        /**
+         * Automata/stack should fix this nicely. 
+         * Not necessary now, TODO
+         /*
+        bool isValidRegex(string expression)
+        {
+            bool isValid = false;
+            return isValid;
+        }
+         /**/
+
+        
+        
+    }
+
+}
